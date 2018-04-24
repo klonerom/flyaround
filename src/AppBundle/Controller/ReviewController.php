92c138199d68929d01f0155c57937507ace71650
romain coquery <coquery.romain@gmail.com>
2018-04-24 22:51:58 +0200

    perso views listing, reservation + debut reviewController, class Review

---
+++ src/AppBundle/Controller/ReviewController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+
+
+/**
+ * Review controller.
+ *
+ * @Route("review")
+ */
+class ReviewController extends Controller
+{
+
+
+
+}
